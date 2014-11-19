529ba9fedc49e734f72a3bfd1abca111faacd23c
Taylor Smith <tsmith@tsmithcreative.com>
2014-11-19 03:18:58 +0000

    Move acquired js files to vendor

--- includes/footer.php
+++ includes/footer.php
@@ -2,8 +2,8 @@
     <pre><code class="language-scss"><?php include 'sass/' . $stylesheet . '.scss'; ?></code></pre>
 
   </div>
-  <script src="js/prism.js"></script>
-  <script src="js/jquery-1.11.1.min.js"></script>
+  <script src="js/vendor/prism.js"></script>
+  <script src="js/vendor/jquery-1.11.1.min.js"></script>
   <script src="js/main.js"></script>
 </body>
 </html>
